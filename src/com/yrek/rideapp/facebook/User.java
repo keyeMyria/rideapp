41724199c776d3c13c759b9ad9676e0637810b57
Q.P.Liu <qpliu@yahoo.com>
2010-08-21 02:05:12 -0700

    Remove unused user fields.

--- src/com/yrek/rideapp/facebook/User.java
+++ src/com/yrek/rideapp/facebook/User.java
@@ -2,10 +2,7 @@ package com.yrek.rideapp.facebook;
 
 import java.io.Serializable;
 
-import javax.xml.bind.annotation.XmlElement;
-
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
-import org.codehaus.jackson.annotate.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown=true)
 public class User implements Serializable {
@@ -13,14 +10,7 @@ public class User implements Serializable {
 
     private String id;
     private String name;
-    private String firstName;
-    private String lastName;
-    private String link;
-    private String gender;
-    private Integer timezone;
-    private String locale;
 
-    @XmlElement
     public String getId() {
         return id;
     }
@@ -29,70 +19,11 @@ public class User implements Serializable {
         this.id = id;
     }
 
-    @XmlElement
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    @XmlElement
-    @JsonProperty("first_name")
-    public String getFirstName() {
-        return firstName;
-    }
-
-    @JsonProperty("first_name")
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    @XmlElement
-    @JsonProperty("last_name")
-    public String getLastName() {
-        return lastName;
-    }
-
-    @JsonProperty("last_name")
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    @XmlElement
-    public String getLink() {
-        return link;
-    }
-
-    public void setLink(String link) {
-        this.link = link;
-    }
-
-    @XmlElement
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    @XmlElement
-    public Integer getTimezone() {
-        return timezone;
-    }
-
-    public void setTimezone(Integer timezone) {
-        this.timezone = timezone;
-    }
-
-    @XmlElement
-    public String getLocale() {
-        return locale;
-    }
-
-    public void setLocale(String locale) {
-        this.locale = locale;
     }
 }
